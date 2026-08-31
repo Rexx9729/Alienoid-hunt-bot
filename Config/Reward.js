82f235ff0554f2ba7a66acbcd66650a5c92040c5
Rexx9729 <guptarishi27918@gmail.com>
2026-08-31 20:49:07 +0530

    Reward.js

---
+++ Config/Reward.js
@@ -0,0 +1,15 @@
+// ==================== ALIENOID REWARDS ====================
+
+module.exports = {
+
+    // ==================== HUNT ====================
+
+    HUNT_LOSE_REWARD: 30,
+
+
+    // ==================== FUTURE DUAL / PVP ====================
+
+    DUAL_WIN_REWARD: 100,
+    DUAL_LOSE_REWARD: 50
+
+};
